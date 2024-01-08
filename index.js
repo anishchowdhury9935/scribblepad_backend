fd831a6e5d037fb042120f72ac6e536167868165
anishchowdhury9935 <142099444+anishchowdhury9935@users.noreply.github.com>
2024-01-08 18:45:48 +0530

    build : update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express  = require('express');
 const cors = require('cors');
 const app = express();
-// const port = process.env.PORT || 5000
-const port = 5000
+const port = process.env.PORT || 5000
+// const port = 5000
 const connectToMongoose = require('./db.js')
 app.use(cors());
 
