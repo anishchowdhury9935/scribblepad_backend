aa7cfadf5653f1d6cc7ce4246a10458562ec331c
anishchowdhury9935 <142099444+anishchowdhury9935@users.noreply.github.com>
2024-02-20 22:30:27 +0530

    Update : db.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 // const host_url ='mongodb://127.0.0.1:27017/inotebook';
-const host_url ='mongodb+srv://anish:anish9935@cluster1.qcwsrsy.mongodb.net/?retryWrites=true&w=majority"';
+const host_url = process.env.dbServer;
 
 const connectToMongo = ()=>{
     mongoose.connect(host_url).then(()=>{
